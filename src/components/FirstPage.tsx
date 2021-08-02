19d8fb1faf994b541a30fcc8d278645d039084e6
Даниил Никитин <faverus061999@gmail.com>
2021-08-02 13:14:25 +0300

    add background video + banner appears after 5 sec

--- src/components/FirstPage.tsx
+++ src/components/FirstPage.tsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "../styles/FirstPage.module.scss";
 import qr from "../img/qrcode.png";
 import { NavLink } from "react-router-dom";
+import video from "../video/video.mp4";
 
 const Banner = () => {
+  const [started, setStarted] = useState(false);
+  const [appear, setAppear] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setAppear(true);
+    }, 5000);
+  }, [started]);
   return (
-    <section className={style.video}>
-      <div className={style.banner}>
+    <section className={style.container}>
+      <video
+        autoPlay
+        muted
+        loop
+        className={style.video}
+        width="1280px"
+        height="720px"
+        onPlay={() => setStarted(true)}
+      >
+        <source src={video} type="video/mp4" />
+      </video>
+      <div className={appear ? style.banner : style.banner2}>
         <h2>
           Используйте мечту вашего малыша! <br />
           Подарите ему собаку!
